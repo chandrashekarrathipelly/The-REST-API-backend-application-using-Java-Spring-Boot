4bef57a8de65546c8577c273da5f028478916360
chandrashekharrathipelly <chandrashekharrathipelly@gmail.com>
2024-08-23 15:40:50 +0530

    Intial Changes

---
+++ JavaBackendRestAPI/src/main/java/com/JavaBackendRestAPI/JavaBackendRestAPI/JavaBackendRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.JavaBackendRestAPI.JavaBackendRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendRestApiApplication.class, args);
+	}
+
+}
